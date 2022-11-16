813b971f16aba61e3ead84bdccee5e9511030523
Leonty Chudinov <leonty@inbox.ru>
2022-11-16 08:08:17 +0500

    Copy mySimPL to MySimPL with type checker
    Signed-off-by: Leonty Chudinov <leonty@inbox.ru>

---
+++ interpreters/mysimpl-with-typechecker/src/main.ml
@@ -0,0 +1,80 @@
+open Ast
+
+let parse (s: string) : expr =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+
+(** [is_value e] is whether [e] is a value *)
+let is_value : expr -> bool = function
+  | Int _ | Bool _ -> true
+  | Var _ | Let _ | Binop _ | If _ -> false
+
+(** [subst e v x] is [e] with [v] substituted for [x], that
+    is [e{v/x}]. *)
+let rec subst e v x = match e with
+  | Var y -> if x = y then v else e
+  | Bool _ -> e
+  | Int _ -> e
+  | Binop (bop, e1, e2) -> Binop (bop, subst e1 v x, subst e2 v x)
+  | Let (y, e1, e2) -> 
+    let e' = subst e1 v x in
+    if y = x then Let (y, e', e2) else Let (y, e', subst e2 v x)
+  | If (e1, e2, e3) -> If (subst e1 v x, subst e2 v x, subst e3 v x)
+
+(** [step] is the [-->] relation, that is, a single step of evaluation. *)
+let rec step : expr -> expr = function
+  | Int _ | Bool _ -> failwith "Does not step"
+  | Var _ -> failwith "Unbound variable"
+  | Binop (bop, e1, e2) when is_value e1 && is_value e2 -> step_bop bop e1 e2
+  | Binop (bop, e1, e2) when is_value e1 -> Binop (bop, e1, step e2)
+  | Binop (bop, e1, e2) when is_value e2 -> Binop (bop, step e1, e2)
+  | Binop (_, _, _) -> failwith "Unhandled binop"
+  | Let (x, e1, e2) when is_value e1 -> subst e2 e1 x
+  | Let (x, e1, e2) -> Let (x, step e1, e2)
+  | If (Bool true, e2, _) -> e2
+  | If (Bool false, _, e3) -> e3
+  | If (Int _, _, _) -> failwith "Guard of if must have type bool"
+  | If (e1, e2, e3) -> If(step e1, e2, e3)
+
+(** [step_bop bop v1 v2] implements the primitive operation
+    [v1 bop v2]. Requires: [v1] and [v2] are both values. *)
+and step_bop bop e1 e2 = match bop, e1, e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith "Operator and operand mismatch"
+
+(** [eval_small e] is the [e -->* v] relation. That is,
+    keep applying [step] until a value is produced. *)
+let rec eval_small (e : expr) : expr =
+  if is_value e then e
+  else e |> step |> eval_small
+  
+(** [eval_big e] is the [e ==> v] relation. *)
+let rec eval_big (e : expr) : expr = match e with
+  | Int _ | Bool _ -> e
+  | Var _ -> failwith "Unbound variable"
+  | Binop (bop, e1, e2) -> eval_bop bop e1 e2
+  | Let (x, e1, e2) -> eval_let x e1 e2
+  | If (e1, e2, e3) -> eval_if e1 e2 e3
+
+and eval_let x e1 e2 = 
+  let v1 = eval_big e1 in
+  let e2' = subst e2 v1 x in
+  eval_big e2'
+
+(** [eval_bop bop e1 e2] is the [e] such that [e1 bop e2 ==> e]. *)
+and eval_bop bop e1 e2 = match bop, eval_big e1, eval_big e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith "Operator and operand type mismatch"
+  
+(** [eval_if e1 e2 e3] is the [e] such that [if e1 then e2 else e3 ==> e]. *)
+and eval_if e1 e2 e3 = match eval_big e1 with
+  | Bool true -> eval_big e2
+  | Bool false -> eval_big e3
+  | Int _ -> failwith "Guard of if must have type bool"
+  | _ -> failwith "precondition violated"
+  
